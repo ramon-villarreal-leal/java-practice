6f87757cdb35e40c642cf4cb31a041e14196b99c
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-09-14 09:40:45 -0500

    for loops again

--- src/FizzBuzz/JavaPractice.java
+++ src/FizzBuzz/JavaPractice.java
@@ -1,7 +1,14 @@
+package FizzBuzz;
+
 public class JavaPractice {
 
 
     public static void main(String[] args) {
         // Practice FizzBuzz in Java
+
+        for(i =0; i <= 100; i++) {
+
+        }
+
     }
 }
