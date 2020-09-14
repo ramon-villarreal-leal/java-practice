c78f5986c64d341e5a3ec23f9681194d64bc5d1d
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-09-14 10:19:08 -0500

    finished fizzBuzz practice

--- src/FizzBuzz/JavaPractice.java
+++ src/FizzBuzz/JavaPractice.java
@@ -6,8 +6,16 @@ public class JavaPractice {
     public static void main(String[] args) {
         // Practice FizzBuzz in Java
 
-        for(i =0; i <= 100; i++) {
-
+        for(int i =0; i <= 100; i +=1) {
+            if(i % 3 ==0 && i % 5 ==0) {
+                System.out.println("fizzBuzz");
+            } else if(i % 5 ==0) {
+                System.out.println("Fizz");
+            } else if(i % 3 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
         }
 
     }
