ea6523e3aeda2610c10bd294064939312d584304
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-09-18 11:45:55 -0500

    expressions

--- src/logicProblems/Logic.java
+++ src/logicProblems/Logic.java
@@ -18,3 +18,4 @@ public class Logic {
         return product - sum;
     }
 }
+
---
+++ src/masterClass/Expressions.java
@@ -0,0 +1,15 @@
+package masterClass;
+
+public class Expressions {
+    public static void main(String[] args) {
+
+
+        int myFirstNumber = (10 + 5) + (2 + 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+
+    }
+}
