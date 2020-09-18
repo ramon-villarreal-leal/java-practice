e9e41bf9e22a789b1ebd55a5815c6d2a8b719e88
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-09-18 11:57:47 -0500

    updating expressions

--- src/masterClass/Expressions.java
+++ src/masterClass/Expressions.java
@@ -4,12 +4,14 @@ public class Expressions {
     public static void main(String[] args) {
 
 
-        int myFirstNumber = (10 + 5) + (2 + 10);
+        int myFirstNumber = (10 + 5) + (2 * 10);
         int mySecondNumber = 12;
-        int myThirdNumber = 6;
+        int myThirdNumber = myFirstNumber * 2;
 
         int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
-        System.out.println(myTotal);
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
 
     }
 }
