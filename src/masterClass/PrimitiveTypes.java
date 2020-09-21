bd1cccdaae8eeba6dfc0a51a26506e211b6eac36
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-09-21 11:42:44 -0500

    updating max and min byte

--- src/masterClass/PrimitiveTypes.java
+++ src/masterClass/PrimitiveTypes.java
@@ -11,5 +11,19 @@ public class PrimitiveTypes {
 
         System.out.println("Integer Minimum Value: " + myMinIntValue);
         System.out.println("Integer Maximum Value: " + myMaxIntValue);
+        //example of an overflow
+        System.out.println("Busted Max value is = " + (myMaxIntValue + 1));
+        //example of underflow it flips to opposite
+        System.out.println("Busted Min value is = " + (myMinIntValue -1));
+        // use appropriate numbers and data  types
+
+        int myMaxIntTest = 2147483647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Byte Maximum Value = " + myMaxByteValue );
+        System.out.println("Byte Minimum Value = " +myMinByteValue );
+
     }
 }
