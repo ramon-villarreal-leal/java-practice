e8bc5da9885ef03e766635c1b60c302d2c797116
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-09-21 13:14:15 -0500

    dogs can bark now

--- src/HeadFirstJava/Methods.java
+++ src/HeadFirstJava/Methods.java
@@ -1,12 +1,12 @@
 package HeadFirstJava;
 
 public class Methods {
-
-   Song t2 = new Song();
-  t2.setArtist("Travis");
-    t2.setTitle("Sing");
-
-    void play(){
-        soundPlayer.playSound(title);
-    }
+//
+//   Song t2 = new Song();
+//  t2.setArtist("Travis");
+//    t2.setTitle("Sing");
+//
+//    void play(){
+//        soundPlayer.playSound(title);
+//    }
 }
