80cb8aa2b94e67a5df7af20522a71e2a48327c5d
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-09-24 07:49:13 -0500

    updating if

--- src/masterClass/Operators.java
+++ src/masterClass/Operators.java
@@ -16,5 +16,23 @@ public class Operators {
         result += 2;
 
         System.out.println(result);
+
+        boolean isAlien = false;
+        if(isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("I am scared of aliens");
+        }
+
+        int topScore = 80;
+        if(topScore < 100) {
+            System.out.println("You got the high score!");
+        } else {
+            System.out.println("No high score!!!! ");
+        }
+
+        int secondTopScore = 60;
+        if(topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than the second top score and less than 100");
+        }
     }
 }
