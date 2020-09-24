412b9616a2ce6fac31da9e004d05ecb7bb563a5d
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-09-24 09:41:51 -0500

    updating op

--- src/masterClass/Operators.java
+++ src/masterClass/Operators.java
@@ -30,9 +30,18 @@ public class Operators {
             System.out.println("No high score!!!! ");
         }
 
-        int secondTopScore = 60;
-        if(topScore > secondTopScore && topScore < 100) {
+        int secondTopScore = 81;
+        if((topScore > secondTopScore) && (topScore < 100)) {
             System.out.println("Greater than the second top score and less than 100");
+        }
+
+        if((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or");
+        }
+
+        int newValue = 50;
+        if(newValue = 50) {
+            System.out.println("This is an error");
         }
     }
 }
