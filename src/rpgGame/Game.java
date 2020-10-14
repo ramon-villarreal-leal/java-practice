de9ee3fb8c4fa6f1b900ee5af4f838395dc3ffce
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-10-14 14:10:38 -0500

    updating game

--- src/rpgGame/Game.java
+++ src/rpgGame/Game.java
@@ -1,6 +1,35 @@
 package rpgGame;
 
 public class Game {
+
+    private int score;
+    private int potions;
+    private int upgradeWeapon;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getPotions() {
+        return potions;
+    }
+
+    public void setPotions(int potions) {
+        this.potions = potions;
+    }
+
+    public int getUpgradeWeapon() {
+        return upgradeWeapon;
+    }
+
+    public void setUpgradeWeapon(int upgradeWeapon) {
+        this.upgradeWeapon = upgradeWeapon;
+    }
+
     public static void main(String[] args) {
 
     }
