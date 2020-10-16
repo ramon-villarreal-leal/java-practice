ef56971278227d2e3669d3e8630c7806233fc1dd
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-10-16 14:48:00 -0500

    adding weapons

---
+++ src/rpgGame/Weapon.java
@@ -0,0 +1,49 @@
+package rpgGame;
+
+public class Weapon {
+    private int sword;
+    private int axe;
+    private int magicWand;
+    private int mace;
+    private int upgradeWeapon;
+
+    public int getUpgradeWeapon() {
+        return upgradeWeapon;
+    }
+
+    public void setUpgradeWeapon(int upgradeWeapon) {
+        this.upgradeWeapon = upgradeWeapon;
+    }
+
+    public int getSword() {
+        return sword;
+    }
+
+    public void setSword(int sword) {
+        this.sword = sword;
+    }
+
+    public int getAxe() {
+        return axe;
+    }
+
+    public void setAxe(int axe) {
+        this.axe = axe;
+    }
+
+    public int getMagicWand() {
+        return magicWand;
+    }
+
+    public void setMagicWand(int magicWand) {
+        this.magicWand = magicWand;
+    }
+
+    public int getMace() {
+        return mace;
+    }
+
+    public void setMace(int mace) {
+        this.mace = mace;
+    }
+}
