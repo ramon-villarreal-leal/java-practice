332a816e8a83b6326c04930c433226fa96b97916
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-10-19 15:25:51 -0500

    updating quests

---
+++ src/rpgGame/Quests.java
@@ -0,0 +1,31 @@
+package rpgGame;
+
+public class Quests {
+    private int gold;
+    private int gems;
+    private int levels;
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    public int getGems() {
+        return gems;
+    }
+
+    public void setGems(int gems) {
+        this.gems = gems;
+    }
+
+    public int getLevels() {
+        return levels;
+    }
+
+    public void setLevels(int levels) {
+        this.levels = levels;
+    }
+}
