59f68826942a3ed3a1e8a6459f9b1cda910e1b77
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-10-20 09:42:07 -0500

    adding difficulty

--- src/rpgGame/Quests.java
+++ src/rpgGame/Quests.java
@@ -4,6 +4,15 @@ public class Quests {
     private int gold;
     private int gems;
     private int levels;
+    private int difficulty;
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
 
     public int getGold() {
         return gold;
