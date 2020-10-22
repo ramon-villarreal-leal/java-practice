0ee7420e4a821a9dd300b3403ad5d37576900aea
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-10-21 22:14:35 -0500

    updating buffs

---
+++ src/rpgGame/Magic.java
@@ -0,0 +1,22 @@
+package rpgGame;
+
+public class Magic {
+    private int buffs;
+    private int debuffs;
+
+    public int getBuffs() {
+        return buffs;
+    }
+
+    public void setBuffs(int buffs) {
+        this.buffs = buffs;
+    }
+
+    public int getDebuffs() {
+        return debuffs;
+    }
+
+    public void setDebuffs(int debuffs) {
+        this.debuffs = debuffs;
+    }
+}
