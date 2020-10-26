7d9c81aad36ebec46c632cb199f41940fed8f1d0
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-10-26 10:54:14 -0500

    removing logic

--- src/rpgGame/Logic.java
+++ src/rpgGame/Logic.java
@@ -3,17 +3,6 @@ package rpgGame;
 
 
 public class Logic {
-    int digitsManipulations(int n) {
-        int product =1;
-        int sum = 0;
 
-        while(n > 0) {
-            sum += n % 10;
-            product *= n % 10;
-            n /= 10;
-
-        }
-        return product - sum;
-    }
 }
 
