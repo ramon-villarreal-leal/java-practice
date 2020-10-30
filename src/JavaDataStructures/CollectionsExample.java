bb65fd970fae512714e1c0a6360607b184795ca9
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-10-30 13:19:49 -0500

    fibonacci example

--- src/JavaDataStructures/CollectionsExample.java
+++ src/JavaDataStructures/CollectionsExample.java
@@ -1,7 +1,13 @@
 package JavaDataStructures;
 
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class CollectionsExample {
     public static void main(String[] args) {
+        List<Integer> list1 = Arrays.asList(1,1,2,3,5,8,13,21,34,55);
 
     }
 }
