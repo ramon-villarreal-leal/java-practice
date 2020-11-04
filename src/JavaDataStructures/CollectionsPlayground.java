d2b1274fa4b61372fd2b7c748955d81c9560d6e1
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-11-03 23:08:04 -0600

    messing around with collections

---
+++ src/JavaDataStructures/CollectionsPlayground.java
@@ -0,0 +1,16 @@
+package JavaDataStructures;
+
+import java.util.ArrayList;
+
+public class CollectionsPlayground {
+    // We'll start by defining an array list and adding some items to it
+    public static void main(String[] args) {
+
+
+        ArrayList<String> roasts = new ArrayList<>();
+        roasts.add("light");
+        roasts.add("medium");
+        roasts.add("medium");
+        roasts.add("dark");
+    }
+}
