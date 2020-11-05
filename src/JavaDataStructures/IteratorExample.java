3f2a7fb48c64ceb5e756abb5c31a0d579235181d
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-11-04 22:29:03 -0600

    iterator update

--- src/JavaDataStructures/IteratorExample.java
+++ src/JavaDataStructures/IteratorExample.java
@@ -2,6 +2,7 @@ package JavaDataStructures;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 
 public class IteratorExample {
@@ -12,6 +13,11 @@ public class IteratorExample {
         while(iterator.hasNext()) {
             System.out.println(iterator.next());
         }
+
+        Collection collection1 = Arrays.asList("Little Big Planet", "Grand Turismo", "DS");
+
+        Iterator iterator1 = collection1.iterator();
+
     }
 
     //Iterator is useful for using ArrayList, LinkedList, Queues and Stacks
