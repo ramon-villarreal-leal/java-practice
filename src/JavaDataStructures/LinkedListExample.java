009544909a8848d0a5c5274669f214167db35853
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-11-11 09:07:32 -0600

    updating states and LinkedList notes

--- src/JavaDataStructures/LinkedListExample.java
+++ src/JavaDataStructures/LinkedListExample.java
@@ -1,7 +1,17 @@
 package JavaDataStructures;
 
+import java.util.LinkedList;
+
 public class LinkedListExample {
     public static void main(String[] args) {
-
+        //a ccess to elements is linear
+        //pointer is at the top of the list
+        //bidirectional list
+        //ListIterator travels backwards through the list
+        LinkedList states = new LinkedList();
+        states.add("Alaska");
+        states.add("Arizona");
+        states.add("Arkansas");
+        states.add("Colorado");
     }
 }
