97e6f465404c1a7db3f5981bcaedae975ecb67ff
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-11-18 23:07:36 -0600

    updating LinkedList

--- src/JavaDataStructures/LinkedListExample.java
+++ src/JavaDataStructures/LinkedListExample.java
@@ -9,11 +9,18 @@ public class LinkedListExample {
         //pointer is at the top of the list
         //bidirectional list
         //ListIterator travels backwards through the list
+
+        //LinkedList is good if you are going to be adding elements to the middle or end of list
+        // adding anf retrieving is the same thing
         LinkedList states = new LinkedList();
         states.add("Alaska");
         states.add("Arizona");
         states.add("Arkansas");
         states.add("Colorado");
+        states.add("California");
+        states.add("Washington");
+        states.add("New York");
+        states.add("New Hampshire");
         //Last item of LinkedList points to null to indicate end of the list
         states.add("Connecticut");
         states.addFirst("Alabama");
@@ -22,6 +29,10 @@ public class LinkedListExample {
         ListIterator iterator = states.listIterator(states.size());
         while(iterator.hasPrevious()) {
             System.out.println(iterator.previous());
+        }
+
+        if(states.contains("California")) {
+            System.out.println("Its a sunny state");
         }
     }
 }
