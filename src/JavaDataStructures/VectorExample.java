c5eb3dae317ed9f91d03de5d7af343a6a83756d7
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-11-19 23:44:15 -0600

    vector playground

---
+++ src/JavaDataStructures/VectorExample.java
@@ -0,0 +1,11 @@
+package JavaDataStructures;
+
+public class VectorExample {
+    //Vectors are not in wide use Most people use ArrayLists
+    String [] platform1 = {"Ps4"};
+    String [] platform2 = {"3DS", "Wii U"};
+
+    VideoGame game1 = new VideoGame("Battlefield 1", 2001, "M", platform1);
+    VideoGame game2 = new VideoGame("Dark Souls", 2009, "M", platform1);
+
+}
