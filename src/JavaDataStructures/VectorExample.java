34ed9204badcdde20a05a58739607e2e004476aa
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-11-19 23:47:40 -0600

    vector update

--- src/JavaDataStructures/VectorExample.java
+++ src/JavaDataStructures/VectorExample.java
@@ -1,11 +1,24 @@
 package JavaDataStructures;
 
-public class VectorExample {
-    //Vectors are not in wide use Most people use ArrayLists
-    String [] platform1 = {"Ps4"};
-    String [] platform2 = {"3DS", "Wii U"};
+import java.util.Vector;
 
-    VideoGame game1 = new VideoGame("Battlefield 1", 2001, "M", platform1);
-    VideoGame game2 = new VideoGame("Dark Souls", 2009, "M", platform1);
+public class VectorExample {
+
+    public static void main(String[] args) {
+
+
+        //Vectors are not in wide use Most people use ArrayLists
+        String[] platform1 = {"Ps4"};
+        String[] platform2 = {"3DS", "Wii U"};
+
+        VideoGame game1 = new VideoGame("Battlefield 1", 2001, "M", platform1);
+        VideoGame game2 = new VideoGame("Dark Souls", 2009, "M", platform1);
+
+        //no datatype specified
+        Vector videoGames = new Vector();
+
+       videoGames.add(game1);
+       videoGames.add(game2);
+    }
 
 }
