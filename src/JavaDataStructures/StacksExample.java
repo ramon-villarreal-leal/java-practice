941bb29ea60241f2bba9fddf7027a554b4a92c13
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-11-23 21:46:14 -0600

    updating a Stack

---
+++ src/JavaDataStructures/StacksExample.java
@@ -0,0 +1,14 @@
+package JavaDataStructures;
+
+public class StacksExample {
+    //Stack uses Last In First out order
+    //Vertical stack of objects
+    // to add an item use .push() to stack
+    // remove at end of item use .pop()
+    // peek method makes a copy of the first item
+
+    //A stack is an easy way to reverse a collection of values
+    public static void main(String[] args) {
+
+    }
+}
