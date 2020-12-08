1f6e1e5576a01a15fda6eae41412e206079d8980
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-12-07 21:30:46 -0600

    using dice exmaple

--- src/JavaInterviewPractice/StaticvsNonStatic/Dice.java
+++ src/JavaInterviewPractice/StaticvsNonStatic/Dice.java
@@ -12,7 +12,7 @@ public class Dice {
         return this.faceValue;
     }
 
-    public static void main(String[] args) {
+    public static void changeNumberOfSides(int newNumberOfSides) {
 
         Dice.sidesOfDice = newNumberOfSides;
     }
--- src/JavaInterviewPractice/StaticvsNonStatic/Main.java
+++ src/JavaInterviewPractice/StaticvsNonStatic/Main.java
@@ -1,4 +1,22 @@
 package JavaInterviewPractice.StaticvsNonStatic;
 
 public class Main {
+    public static void main(String[] args) {
+        // creating the dice object
+        System.out.println(Dice.sidesOfDice);
+        Dice d = new Dice();
+        Dice otherDice = new Dice();
+
+        //rolling a 6 sided die
+        System.out.println();
+        System.out.println("Six Sides");
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println("First die: " + d.roll());
+            System.out.println("Second Die: " + otherDice.roll());
+        }
+
+        //Retrieving face value
+
+    }
 }
