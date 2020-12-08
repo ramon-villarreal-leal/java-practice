40ff038f940f57d9ee97b2e17f2591891588aa28
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-12-07 21:10:43 -0600

    java notes for interview

---
+++ src/JavaInterviewPractice/Practice.java
@@ -0,0 +1,15 @@
+package JavaInterviewPractice;
+
+public class Practice {
+    // Java Virtual Machine (JVM)
+    // describes a set of instructions, registers, stack, heap, and method area
+    // after compilation its called byte code
+    // No jvm implmentation is the same
+    // a class like Dog has instance variables like German Shepherd or Shih Tzu that represents instances of the Dog breed\//
+
+    //static and non static method
+    //Static
+    // Belongs to class itself
+    // Do not need an instance in order to use a static method
+    //Method depends on class
+}
