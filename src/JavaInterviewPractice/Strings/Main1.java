a47f101eb511f3b6563aa9ed5492163ff3f9944f
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-12-15 10:23:16 -0600

    updating

--- src/JavaInterviewPractice/Strings/Main1.java
+++ src/JavaInterviewPractice/Strings/Main1.java
@@ -2,6 +2,13 @@ package JavaInterviewPractice.Strings;
 
 public class Main1 {
     public static void main(String[] args) {
+        String p = " Panda";
+        String f = " Fish";
+        String h = "Horse ";
+        String c = " Cat";
+
+        String nothing = " ";
+
 
     }
 }
