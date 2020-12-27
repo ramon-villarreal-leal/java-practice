a999ed206bfb5d06e5bd510846befe11481eb241
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-12-26 23:30:28 -0600

    updating hero

--- src/rpgGame/Logic.java
+++ src/rpgGame/Logic.java
@@ -4,5 +4,11 @@ package rpgGame;
 
 public class Logic {
 
+    public static void main(String[] args) {
+        System.out.println("Lets start a game");
+        Hero hero1 = new Hero();
+        Weapon weapon1 = new Weapon();
+        hero1.setAttack(45);
+    }
 }
 
