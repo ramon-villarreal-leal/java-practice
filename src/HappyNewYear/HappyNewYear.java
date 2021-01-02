a23eb520e5613fc975de9c80ca43cc59719807f8
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-01-01 20:21:03 -0600

    updating

--- src/HappyNewYear/HappyNewYear.java
+++ src/HappyNewYear/HappyNewYear.java
@@ -1,4 +1,26 @@
 package HappyNewYear;
 
 public class HappyNewYear {
+    private String happy;
+    private String newYear;
+
+    public String getHappy() {
+        return happy;
+    }
+
+    public void setHappy(String happy) {
+        this.happy = happy;
+    }
+
+    public String getNewYear() {
+        return newYear;
+    }
+
+    public void setNewYear(String newYear) {
+        this.newYear = newYear;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
