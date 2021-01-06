9efee26f257676421f8ea5502a6d923282552c77
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-01-06 11:26:05 -0600

    updating

--- src/rpgGame/TestGame.java
+++ src/rpgGame/TestGame.java
@@ -2,4 +2,21 @@ package rpgGame;
 
 public class TestGame {
     private String gameText;
+    private String prompts;
+
+    public String getGameText() {
+        return gameText;
+    }
+
+    public void setGameText(String gameText) {
+        this.gameText = gameText;
+    }
+
+    public String getPrompts() {
+        return prompts;
+    }
+
+    public void setPrompts(String prompts) {
+        this.prompts = prompts;
+    }
 }
