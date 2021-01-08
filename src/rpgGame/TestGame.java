9fc1e006aeaeccc1539a24eb6c6d4eeab27c7061
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-01-08 14:04:48 -0600

    checking if git works

--- src/rpgGame/TestGame.java
+++ src/rpgGame/TestGame.java
@@ -4,6 +4,7 @@ public class TestGame {
     private String gameText;
     private String prompts;
     private String test1;
+    private String test2;
 
     public String getGameText() {
         return gameText;
@@ -27,5 +28,13 @@ public class TestGame {
 
     public void setTest1(String test1) {
         this.test1 = test1;
+    }
+
+    public String getTest2() {
+        return test2;
+    }
+
+    public void setTest2(String test2) {
+        this.test2 = test2;
     }
 }
