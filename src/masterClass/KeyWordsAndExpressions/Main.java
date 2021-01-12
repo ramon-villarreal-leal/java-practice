333c8c5131ce031649a48f5b11004c817102bf04
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-01-12 13:10:33 -0600

    updating

---
+++ src/masterClass/KeyWordsAndExpressions/Main.java
@@ -0,0 +1,10 @@
+package masterClass.KeyWordsAndExpressions;
+
+public class Main {
+    // Expressions are building blocks of all Java programs
+    //a mile is 1.609344 km
+    double kilometers = (100 * 1.609344);
+
+
+}
+
