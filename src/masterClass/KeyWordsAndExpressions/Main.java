adfc630a316b63265854c0863b99a3830ab68a6b
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-01-12 13:58:19 -0600

    updating

--- src/masterClass/KeyWordsAndExpressions/Main.java
+++ src/masterClass/KeyWordsAndExpressions/Main.java
@@ -11,19 +11,33 @@ public class Main {
 
         int highScore = 50;
 
-        if(highScore == 50) {
+        if (highScore == 50) {
             System.out.println("an Expression!");
             System.out.println("Hello Friendo!");
         }
 
         int score = 200;
-        if(score > 100) {
+        if (score > 100) {
             System.out.println("You got the highj score man!");
         } else {
             System.out.println("No more high score dude");
         }
 
-    }
 
+        int myVariable = 50;
+        myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a cool test");
+        System.out.println("This is a cooler test" +
+                "More test"
+        + "Lets goooo");
+
+
+        int anotherVar = 50;
+        myVariable--;
+        System.out.println("Another one for the books");
+
+    }
 }
 
