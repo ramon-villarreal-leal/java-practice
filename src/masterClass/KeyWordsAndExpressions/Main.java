7dda5f9c42c3a5a5296daac36db8d8a549b1181d
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-01-12 13:18:09 -0600

    updating

--- src/masterClass/KeyWordsAndExpressions/Main.java
+++ src/masterClass/KeyWordsAndExpressions/Main.java
@@ -1,10 +1,27 @@
 package masterClass.KeyWordsAndExpressions;
 
 public class Main {
-    // Expressions are building blocks of all Java programs
-    //a mile is 1.609344 km
-    double kilometers = (100 * 1.609344);
 
+    public static void main(String[] args) {
+
+
+        // Expressions are building blocks of all Java programs
+        //a mile is 1.609344 km
+        double kilometers = (100 * 1.609344);
+
+        int highScore = 50;
+
+        if(highScore == 50) {
+            System.out.println("an Expression!");
+            System.out.println("Hello Friendo!");
+        }
+
+        int score = 200;
+        if(score > 100) {
+            System.out.println("You got the highj score man!");
+        }
+
+    }
 
 }
 
