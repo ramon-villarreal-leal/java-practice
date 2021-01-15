a1fb3cb5d4766c2886302b8cdec6b087b6d10e73
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-01-14 23:50:03 -0600

    updating

--- src/masterClass/ControlStatementsPractice/Main.java
+++ src/masterClass/ControlStatementsPractice/Main.java
@@ -2,6 +2,13 @@ package masterClass.ControlStatementsPractice;
 
 public class Main {
     public static void main(String[] args) {
+        boolean gameOn = true;
+        int score = 6000;
+        int levelComplete = 5;
+        int bonusLevel = 100;
 
+        if(score == 6000) {
+            System.out.println("Your score is 6000");
+        }
     }
 }
