5bdf4c900684d051ed9b748d36ff623031145808
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-01-20 23:25:04 -0600

    updating game

--- src/masterClass/ControlStatementsPractice/Main.java
+++ src/masterClass/ControlStatementsPractice/Main.java
@@ -7,14 +7,21 @@ public class Main {
         int levelComplete = 5;
         int bonusLevel = 100;
 
-        if(score < 4000 && score > 1500) {
-            System.out.println("Your score is 6000");
-            System.out.println("This runs");
-        } else if(score > 2000) {
-            System.out.println("Score was greater than 2000");
+//        if(score < 4000 && score > 1500) {
+//            System.out.println("Your score is 6000");
+//            System.out.println("This runs");
+//        } else if(score > 2000) {
+//            System.out.println("Score was greater than 2000");
+//        } else {
+//            System.out.println("condition false");
+//            System.out.println("Part of condition");
+//        }
+
+        if(gameOn = true) {
+            int finalScoreDude = score + (levelComplete * bonusLevel);
+            System.out.println("The final score is " + finalScoreDude);
         } else {
-            System.out.println("condition false");
-            System.out.println("Part of condition");
+            System.out.println("Game over Bromigo!!!!");
         }
     }
 }
