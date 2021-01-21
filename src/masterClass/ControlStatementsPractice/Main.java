43a232d87a6627f2923637e24f00d06c605e32b9
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-01-20 23:21:52 -0600

    updating

--- src/masterClass/ControlStatementsPractice/Main.java
+++ src/masterClass/ControlStatementsPractice/Main.java
@@ -7,11 +7,11 @@ public class Main {
         int levelComplete = 5;
         int bonusLevel = 100;
 
-        if(score == 4000) {
+        if(score < 4000 && score > 1500) {
             System.out.println("Your score is 6000");
             System.out.println("This runs");
-        } else if(score < 2000) {
-
+        } else if(score > 2000) {
+            System.out.println("Score was greater than 2000");
         } else {
             System.out.println("condition false");
             System.out.println("Part of condition");
