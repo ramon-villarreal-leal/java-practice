affde8b51f7f3ef6b1e388a22fb3dae4a1ff8240
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-01-22 18:31:20 -0600

    method inserted

--- src/masterClass/ControlStatementsPractice/Main.java
+++ src/masterClass/ControlStatementsPractice/Main.java
@@ -6,7 +6,7 @@ public class Main {
         int score = 6000;
         int levelComplete = 5;
         int bonusLevel = 100;
-
+//
 //        if(score < 4000 && score > 1500) {
 //            System.out.println("Your score is 6000");
 //            System.out.println("This runs");
@@ -42,6 +42,20 @@ public class Main {
     }
 
     public static void calculateGameScore() {
+        boolean gameOn = true;
+        int score = 6000;
+        int levelComplete = 5;
+        int bonusLevel = 100;
+
+        if(score < 4000 && score > 1500) {
+            System.out.println("Your score is 6000");
+            System.out.println("This runs");
+        } else if(score > 2000) {
+            System.out.println("Score was greater than 2000");
+        } else {
+            System.out.println("condition false");
+            System.out.println("Part of condition");
+        }
 
     }
 }
