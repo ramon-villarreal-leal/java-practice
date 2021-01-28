90874fa4e81ebd33da9c0973b45abf2055df33d4
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-01-27 19:48:10 -0600

    updating test ground class

--- src/masterClass/ControlStatementsPractice/Main.java
+++ src/masterClass/ControlStatementsPractice/Main.java
@@ -7,7 +7,8 @@ public class Main {
         int levelComplete = 5;
         int bonusLevel = 100;
         calculateGameScore(true, 1000, levelComplete, bonusLevel);
-        calculateGameScore(false, 2500, levelComplete, bonusLevel);
+        //do not define again
+//        calculateGameScore(false, 2500, levelComplete, bonusLevel);
 //
 //        if(score < 4000 && score > 1500) {
 //            System.out.println("Your score is 6000");
---
+++ src/masterClass/ControlStatementsPractice/TestGround.java
@@ -0,0 +1,7 @@
+package masterClass.ControlStatementsPractice;
+
+public class TestGround {
+    public static void main(String[] args) {
+
+    }
+}
