5f1d9f2c5322860c21f9b08932b13521efa1c1a5
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-01-27 19:53:07 -0600

    updating

--- src/masterClass/ControlStatementsPractice/TestGround.java
+++ src/masterClass/ControlStatementsPractice/TestGround.java
@@ -2,6 +2,18 @@ package masterClass.ControlStatementsPractice;
 
 public class TestGround {
     public static void main(String[] args) {
+        boolean game1 = true;
+        int score1 = 700;
+        int levelComplete = 30;
+        int bonusLevel = 600;
 
+        if (game1) {
+            int finalScoreDude = score1 + (levelComplete * bonusLevel * bonusLevel);
+            System.out.println("The final score is " + finalScoreDude);
+        } else {
+            System.out.println("Game over!!!!");
+        }
     }
+
 }
+
