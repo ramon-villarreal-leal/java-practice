16d782cd848dc12d2643dea43bfee0b0053c35a6
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-01-28 23:23:57 -0600

    updating

--- src/masterClass/ControlStatementsPractice/Main1.java
+++ src/masterClass/ControlStatementsPractice/Main1.java
@@ -3,7 +3,25 @@ package masterClass.ControlStatementsPractice;
 public class Main1 {
     private String games;
     private String cheese;
+
+
     public static void main(String[] args) {
         System.out.println("Testing ground");
+    }
+
+    public String getGames() {
+        return games;
+    }
+
+    public void setGames(String games) {
+        this.games = games;
+    }
+
+    public String getCheese() {
+        return cheese;
+    }
+
+    public void setCheese(String cheese) {
+        this.cheese = cheese;
     }
 }
