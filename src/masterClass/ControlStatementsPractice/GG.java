32a0c2e07b8b7072a745f7766cb52a29bbe07580
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-02-02 23:58:27 -0600

    updating

--- src/masterClass/ControlStatementsPractice/GG.java
+++ src/masterClass/ControlStatementsPractice/GG.java
@@ -2,6 +2,9 @@ package masterClass.ControlStatementsPractice;
 
 public class GG {
     public static void main(String[] args) {
-
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Testing");
+            System.out.println(i);
+        }
     }
 }
