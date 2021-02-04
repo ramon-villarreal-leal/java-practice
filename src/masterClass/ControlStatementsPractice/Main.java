4116ef6efa0d3b765a9d4d3cfe2366b18bdd062c
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-02-03 22:37:57 -0600

    updating

--- src/masterClass/ControlStatementsPractice/Main.java
+++ src/masterClass/ControlStatementsPractice/Main.java
@@ -20,7 +20,7 @@ public class Main {
 //            System.out.println("Part of condition");
 //        }
         // Scope of Java code and deals with accessbility!
-        if(gameOn) {
+        if (gameOn) {
             int finalScoreDude = score + (levelComplete * bonusLevel);
             System.out.println("The final score is " + finalScoreDude);
         } else {
@@ -32,7 +32,7 @@ public class Main {
         int newLevelComplete = 8;
         int newBonusLevel = 400;
 
-        if(newGame) {
+        if (newGame) {
             int newFinalScoreDude = newScore + (newLevelComplete * newBonusLevel);
             System.out.println("the new final score is " + newFinalScoreDude);
         } else {
@@ -40,28 +40,25 @@ public class Main {
         }
 
 
-
-
     }
 
 
     // Parameters defined in section of new method
     public static void calculateGameScore(boolean gameOn, int score, int levelComplete, int bonusLevel) {
-        boolean gameOn = true;
-        int score = 6000;
-        int levelComplete = 5;
-        int bonusLevel = 100;
+        gameOn = true;
+        score = 6000;
+        levelComplete = 5;
+        bonusLevel = 100;
 
-        if(score < 4000 && score > 1500) {
+        if (score < 4000 && score > 1500) {
             System.out.println("Your score is 6000");
             System.out.println("This runs");
-        } else if(score > 2000) {
+        } else if (score > 2000) {
             System.out.println("Score was greater than 2000");
 
-        } else if(score < 1000){
+        } else if (score < 1000) {
             System.out.println("Score was less than 1000");
-        }
-        else {
+        } else {
             System.out.println("condition false");
             System.out.println("Part of condition");
             System.out.println("Scope defined");
