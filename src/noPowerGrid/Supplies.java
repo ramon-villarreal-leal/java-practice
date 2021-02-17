79135c77238763950a07bb8e11855c1a576d111a
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-02-17 13:49:29 -0600

    updating

--- src/noPowerGrid/Supplies.java
+++ src/noPowerGrid/Supplies.java
@@ -4,4 +4,28 @@ public class Supplies {
     String water;
     String toiletPaper;
     String backPack;
+
+    public String getWater() {
+        return water;
+    }
+
+    public void setWater(String water) {
+        this.water = water;
+    }
+
+    public String getToiletPaper() {
+        return toiletPaper;
+    }
+
+    public void setToiletPaper(String toiletPaper) {
+        this.toiletPaper = toiletPaper;
+    }
+
+    public String getBackPack() {
+        return backPack;
+    }
+
+    public void setBackPack(String backPack) {
+        this.backPack = backPack;
+    }
 }
