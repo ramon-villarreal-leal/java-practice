0450bd483f203077b8c8fafa1d9dadc842b55a61
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-02-20 21:09:18 -0600

    updating

--- src/noPowerGrid/Food.java
+++ src/noPowerGrid/Food.java
@@ -1,7 +1,33 @@
 package noPowerGrid;
 
 public class Food {
+    private String shumai;
+    private String water;
+    private String craftBeer;
 
+    public String getShumai() {
+        return shumai;
+    }
+
+    public void setShumai(String shumai) {
+        this.shumai = shumai;
+    }
+
+    public String getWater() {
+        return water;
+    }
+
+    public void setWater(String water) {
+        this.water = water;
+    }
+
+    public String getCraftBeer() {
+        return craftBeer;
+    }
+
+    public void setCraftBeer(String craftBeer) {
+        this.craftBeer = craftBeer;
+    }
 
     public static void main(String[] args) {
 
