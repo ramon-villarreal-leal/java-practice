ee75183bb1ea914ea73b0535f42cbae4b2262854
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-02-24 22:54:17 -0600

    updating

--- src/noPowerGrid/Utilities.java
+++ src/noPowerGrid/Utilities.java
@@ -1,6 +1,9 @@
 package noPowerGrid;
 
 public class Utilities {
+    private int electricity;
+    private int gallonsOfWater;
+    private String backPack;
     public static void main(String[] args) {
 
     }
