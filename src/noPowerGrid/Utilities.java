8ddb9f867587186d1f0eb211daecc6e47ffb770a
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-02-24 22:54:48 -0600

    updating

--- src/noPowerGrid/Utilities.java
+++ src/noPowerGrid/Utilities.java
@@ -4,7 +4,29 @@ public class Utilities {
     private int electricity;
     private int gallonsOfWater;
     private String backPack;
-    public static void main(String[] args) {
 
+
+    public int getElectricity() {
+        return electricity;
+    }
+
+    public void setElectricity(int electricity) {
+        this.electricity = electricity;
+    }
+
+    public int getGallonsOfWater() {
+        return gallonsOfWater;
+    }
+
+    public void setGallonsOfWater(int gallonsOfWater) {
+        this.gallonsOfWater = gallonsOfWater;
+    }
+
+    public String getBackPack() {
+        return backPack;
+    }
+
+    public void setBackPack(String backPack) {
+        this.backPack = backPack;
     }
 }
