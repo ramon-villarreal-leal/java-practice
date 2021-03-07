0cc7ebf0d5d957129d93c911bec51b5c5c5c818c
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-03-06 23:33:56 -0600

    updating

--- src/rpgGame/Barbarian.java
+++ src/rpgGame/Barbarian.java
@@ -2,6 +2,8 @@ package rpgGame;
 
 public class Barbarian {
     private String weapons;
+    private String mace;
+
 
     public String getWeapons() {
         return weapons;
@@ -10,6 +12,8 @@ public class Barbarian {
     public void setWeapons(String weapons) {
         this.weapons = weapons;
     }
+
+
 
     public static void main(String[] args) {
 
