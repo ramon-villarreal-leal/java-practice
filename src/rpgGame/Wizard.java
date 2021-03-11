8276c4d599d0f262de4ff212674dbbd94bc8d929
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-03-10 22:40:07 -0600

    updating

--- src/rpgGame/Wizard.java
+++ src/rpgGame/Wizard.java
@@ -4,4 +4,28 @@ public class Wizard {
     private int damage;
     private String Wand;
     private String Staff;
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getWand() {
+        return Wand;
+    }
+
+    public void setWand(String wand) {
+        Wand = wand;
+    }
+
+    public String getStaff() {
+        return Staff;
+    }
+
+    public void setStaff(String staff) {
+        Staff = staff;
+    }
 }
