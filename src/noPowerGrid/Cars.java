aa3e302b9f3f95ea009b5cd4cf797da68d2e2d14
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-03-23 23:36:14 -0500

    adding getters and setters

--- src/noPowerGrid/Cars.java
+++ src/noPowerGrid/Cars.java
@@ -5,4 +5,27 @@ public class Cars {
     private String ac;
     private String wheels;
 
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public String getAc() {
+        return ac;
+    }
+
+    public void setAc(String ac) {
+        this.ac = ac;
+    }
+
+    public String getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(String wheels) {
+        this.wheels = wheels;
+    }
 }
