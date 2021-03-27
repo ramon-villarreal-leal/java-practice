682f02a21ae66755a0483106636cbd269303f4c1
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-03-26 23:28:26 -0500

    updating

--- src/rpgGame/Knight.java
+++ src/rpgGame/Knight.java
@@ -5,5 +5,27 @@ public class Knight {
     private int attack;
     private int defense;
 
+    public String getLance() {
+        return lance;
+    }
 
+    public void setLance(String lance) {
+        this.lance = lance;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
 }
