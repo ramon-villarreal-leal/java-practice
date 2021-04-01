24054e23c18b497730fecef720b8ef8bc7919a99
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-03-31 23:44:19 -0500

    adding getter n setters

--- src/rpgGame/Druid.java
+++ src/rpgGame/Druid.java
@@ -5,6 +5,29 @@ public class Druid {
     private int defense;
     private String Axe;
 
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public String getAxe() {
+        return Axe;
+    }
+
+    public void setAxe(String axe) {
+        Axe = axe;
+    }
 
     public Druid(int attack, int defense, String axe) {
         this.attack = attack;
