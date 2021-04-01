61c22f8f1f1e58aedd5ef11a43af568421eb3d56
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-03-31 23:42:52 -0500

    updating

--- src/rpgGame/Druid.java
+++ src/rpgGame/Druid.java
@@ -5,4 +5,10 @@ public class Druid {
     private int defense;
     private String Axe;
 
+
+    public Druid(int attack, int defense, String axe) {
+        this.attack = attack;
+        this.defense = defense;
+        Axe = axe;
+    }
 }
