bd1b15c7efc57fc7facde560f7316482443a9777
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-04-03 17:43:57 -0500

    updating getter'

--- src/rpgGame/Elf.java
+++ src/rpgGame/Elf.java
@@ -4,5 +4,19 @@ public class Elf {
     private int attack;
     private int defense;
 
+    public int getAttack() {
+        return attack;
+    }
 
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
 }
