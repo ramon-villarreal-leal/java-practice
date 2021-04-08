a51746286f3cb5d9272d9cfcc7f5b80f341db57f
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-04-08 15:08:01 -0500

    updating

--- src/rpgGame/Orc.java
+++ src/rpgGame/Orc.java
@@ -1,6 +1,10 @@
 package rpgGame;
 
 public class Orc {
+    private int attack;
+    private int defense;
+    private int sword;
+    private int shield;
 
 
     public static void main(String[] args) {
