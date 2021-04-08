d74be9ed338e5d510c7a0fac072bd0b33e13a97e
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-04-08 15:10:13 -0500

    getter and setter

--- src/rpgGame/Orc.java
+++ src/rpgGame/Orc.java
@@ -6,6 +6,37 @@ public class Orc {
     private int sword;
     private int shield;
 
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getSword() {
+        return sword;
+    }
+
+    public void setSword(int sword) {
+        this.sword = sword;
+    }
+
+    public int getShield() {
+        return shield;
+    }
+
+    public void setShield(int shield) {
+        this.shield = shield;
+    }
 
     public static void main(String[] args) {
 
