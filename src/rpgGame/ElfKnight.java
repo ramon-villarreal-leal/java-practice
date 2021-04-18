edb1bc3de1ccf1924562f44a3a95f381013375ee
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-04-17 23:59:32 -0500

    updating

---
+++ src/rpgGame/ElfKnight.java
@@ -0,0 +1,13 @@
+package rpgGame;
+
+public class ElfKnight {
+    private int attack;
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+}
