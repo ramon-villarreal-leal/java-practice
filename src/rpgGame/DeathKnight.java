cf948b17ddf4f54e5aac1a8b86451a5b3c3f92ca
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2021-04-23 13:16:45 -0500

    getting and setter

--- src/rpgGame/DeathKnight.java
+++ src/rpgGame/DeathKnight.java
@@ -6,4 +6,35 @@ public class DeathKnight {
     private int magicPower;
     private String weapon;
 
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getMagicPower() {
+        return magicPower;
+    }
+
+    public void setMagicPower(int magicPower) {
+        this.magicPower = magicPower;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
 }
